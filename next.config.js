40aeb522a16e5e14b37b23a1d4a9fd4047733d95
Lionel Dabee <dabee.lionel@gmail.com>
2021-09-07 12:44:16 +0200

    new test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  webpack: (config) => {
+    config.externals = { 'sqlite3': 'commonjs sqlite3', }
+    return config
+  },
   images: {
     domains: ['upload.wikimedia.org', 'cdn.icon-icons.com', 'images.unsplash.com', 'image.pngaaa.com']
   }
